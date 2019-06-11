4286276aa3861ed77184166cfc303562245cc9dc
Rafal <Rafal.Wywrot@onet.pl>
2019-06-11 19:42:16 +0200

    publish house front logic with views

---
+++ Library.WebApplication/Controllers/PublishHouseController.cs
@@ -0,0 +1,51 @@
+﻿using Library.WebApplication.Helpers;
+using Library.WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Library.WebApplication.Controllers
+{
+    public class PublishHouseController : Controller
+    {
+        public ActionResult Index()
+        {
+            var publishHouses = new ApiClient().GetData<List<PublishHouseViewModel>>("publishHouse/GetAll");
+            return View(publishHouses);
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(PublishHouseViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            new ApiClient().PostData<PublishHouseViewModel>("publishHouse/New", model);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var artist = new ApiClient().GetData<PublishHouseViewModel>(String.Format("publishHouse/GetById?id={0}", id));
+            return View(artist);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PublishHouseViewModel teacher)
+        {
+            new ApiClient().PostData<PublishHouseViewModel>("publishHouse/Update", teacher);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            new ApiClient().PostData<DeleteById>("publishHouse/Remove", new DeleteById() { Id = id });
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Library.WebApplication/Models/PublishHouseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.WebApplication.Models
+{
+    public class PublishHouseViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
