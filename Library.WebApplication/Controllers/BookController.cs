ca57fb5b52423a49234936229ecdca60e5044b3a
Rafal <Rafal.Wywrot@onet.pl>
2019-06-11 23:19:37 +0200

    book controller

---
+++ Library.WebApplication/Controllers/BookController.cs
@@ -0,0 +1,72 @@
+﻿using Library.WebApplication.Helpers;
+using Library.WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Library.WebApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        public ActionResult Index()
+        {
+            var books = new ApiClient().GetData<List<BookViewModel>>("book/GetAll");
+            return View(books);
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            var model = new BookEditViewModel();
+            SetBookDropdownOptions(model);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Add(BookEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                SetBookDropdownOptions(model);
+                return View(model);
+            }
+
+            new ApiClient().PostData<BookEditViewModel>("book/New", model);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var book = new ApiClient().GetData<BookEditViewModel>(String.Format("book/GetById?id={0}", id));
+            SetBookDropdownOptions(book);
+            return View(book);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(BookEditViewModel book)
+        {
+            if (!ModelState.IsValid)
+            {
+                SetBookDropdownOptions(book);
+                return View(book);
+            }
+
+            new ApiClient().PostData<BookEditViewModel>("book/Update", book);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            new ApiClient().PostData<DeleteById>("book/Remove", new DeleteById() { Id = id });
+            return RedirectToAction("Index");
+        }
+        private void SetBookDropdownOptions(BookEditViewModel model)
+        {
+            var publishHouses = new ApiClient().GetData<List<PublishHouseViewModel>>("publishHouse/GetAll");
+            var artists = new ApiClient().GetData<List<ArtistViewModel>>("artist/GetAll");
+            var categories = new ApiClient().GetData<List<CategoryViewModel>>("category/GetAll");
+            model.PublishHouses = publishHouses.ToSelectListItems(x => x.Id, x => x.Name);
+            model.Artists = artists.ToSelectListItems(x => x.Id, x => x.Name);
+            model.Categories = categories.ToSelectListItems(x => x.Id, x => x.Name);
+        }
+    }
+}
---
+++ Library.WebApplication/Helpers/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.WebApplication.Helpers
+{
+    public static class Extensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItems<TType, TKey>(this IEnumerable<TType> items,
+             Func<TType, TKey> key,
+             Func<TType, string> text)
+        {
+            return items.Select(item =>
+                new SelectListItem
+                {
+                    Text = text(item),
+                    Value = key(item).ToString()
+                });
+        }
+    }
+}
