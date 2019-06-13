b53c5e2c54cd32e80206ee625a7f746318084c38
Rafal <Rafal.Wywrot@onet.pl>
2019-06-14 00:10:33 +0200

    add rent controller with index view

---
+++ Library.WebApplication/Controllers/RentController.cs
@@ -0,0 +1,32 @@
+﻿using Library.WebApplication.Helpers;
+using Library.WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Library.WebApplication.Controllers
+{
+    public class RentController : Controller
+    {
+        public ActionResult Index()
+        {
+            var rents = new ApiClient().GetData<List<RentViewModel>>("rent/GetAll");
+            return View(rents);
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(RentViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            new ApiClient().PostData<RentViewModel>("rent/New", model);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Library.WebApplication/Models/RentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.WebApplication.Models
+{
+    public class RentViewModel
+    {
+        public int Id { get; set; }
+        public BookViewModel Book { get; set; }
+        public BookEditViewModel BookEdit { get; set; }
+        public bool Available { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+    }
+}
